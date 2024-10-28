22396fd85f14cb4b05a4fb1e120591d6b4c7d7ce
Duc Huy <chuahetheky@gmail.com>
2024-10-28 15:48:33 +0700

    [fix] rename route track

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const tracksRoute = require('./tracksRoute');
 const playlistsRoute = require('./playlistsRoute'); 
 
 function route(app) {
-    app.use('/songs', tracksRoute); 
+    app.use('/tracks', tracksRoute); 
     app.use('/playlists', playlistsRoute); 
 }
 
